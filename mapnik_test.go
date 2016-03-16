db9914c4ffa09a41f6a33ad0816bae1648cdc5b0
sgelb <stephan.halbritter@haw-hamburg.de>
2016-03-16 18:28:15 +0100

    more (very basic) tests

--- mapnik_test.go
+++ mapnik_test.go
@@ -29,6 +29,58 @@ func TestMap(t *testing.T) {
 	if img.Rect.Dx() != 800 || img.Rect.Dy() != 600 {
 		t.Error("unexpected size of output image: ", img.Rect)
 	}
+
+	m.Free()
+	if m.m != nil {
+		t.Error("deallocated map should be nil")
+	}
+}
+
+func TestMapLoad(t *testing.T) {
+	m := New()
+	xml, _ := ioutil.ReadFile("test/map.xml")
+	if err := m.LoadString(string(xml), "test"); err != nil {
+		t.Fatal(err)
+	}
+
+	m.ZoomAll()
+	img, err := m.RenderImage(RenderOpts{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if img.Rect.Dx() != 800 || img.Rect.Dy() != 600 {
+		t.Error("unexpected size of output image: ", img.Rect)
+	}
+
+	m.Free()
+	if m.m != nil {
+		t.Error("deallocated map should be nil")
+	}
+}
+
+func TestDatasource(t *testing.T) {
+	p := map[string]string{"file": "test/test_track.gpx", "layer": "tracks", "type": "ogr"}
+	d := NewDatasource(p)
+	if d.ds == nil {
+		t.Error("did not create new datasource")
+	}
+
+	d.Free()
+	if d.ds != nil {
+		t.Error("deallocated database should be nil")
+	}
+}
+
+func TestLayer(t *testing.T) {
+	l := NewLayer("test", "+init=epsg:4326")
+	if l.l == nil {
+		t.Error("did not create new layer")
+	}
+
+	l.Free()
+	if l.l != nil {
+		t.Error("deallocated layer should be nil")
+	}
 }
 
 func TestRenderFile(t *testing.T) {
@@ -76,7 +128,7 @@ func TestRenderInvalidFormat(t *testing.T) {
 
 func TestSRS(t *testing.T) {
 	m := New()
-	// default mapnil srs
+	// default mapnik srs
 	if srs := m.SRS(); srs != "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" {
 		t.Fatal("unexpected default srs:", srs)
 	}
@@ -90,6 +142,21 @@ func TestSRS(t *testing.T) {
 	if m.SRS() != "+init=epsg:3857" {
 		t.Error("unexpeced srs: ", m.SRS())
 	}
+}
+
+func TestAspectFixMode(t *testing.T) {
+	m := New()
+
+	if m.AspectFixMode() != GrowBBox {
+		t.Error("unexpeced default fixmode: ", m.AspectFixMode())
+	}
+
+	m.SetAspectFixMode(Respect)
+
+	if m.AspectFixMode() != Respect {
+		t.Error("unexpeced fixmode: ", m.AspectFixMode())
+	}
+
 }
 
 func TestBackgroundColor(t *testing.T) {
